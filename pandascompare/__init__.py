6c4c3c93515047068d20cf42c05acc31e6b025a3
Zachary Einck <zacharyeinck@gmail.com>
2025-04-28 23:25:32 -0500

    update version to 0.5.0

--- pandascompare/__init__.py
+++ pandascompare/__init__.py
@@ -1,31 +1,4 @@
-from .coerce import (
-    coerce_series,
-    coerce_dataframe,
-    with_series_coercion,
-    with_dataframe_coercion
-    )
-
-from .combine import (
-    merge_dfs,
-    merge_left_only,
-    join_left_only
-    )
-
 from .compare import PandasCompare
 
-from .decorators import (
-    ignore_nan,
-    inplace_wrapper
-    )
-
-from .dropna import dropna_edges
-
-from .dupes import (
-    drop_duplicates,
-    verify_no_duplicates
-    )
-
-from .utils import *
-
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 __author__ = 'Zachary Einck <zacharyeinck@gmail.com>'
